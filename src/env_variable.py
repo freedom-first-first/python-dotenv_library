1e62c058eabdd4ecb485ebdc89ff54078ebdc1f5
freedom-first-first <cod3freedom@gmail.com>
2025-09-29 17:43:33 +0100

    explaining the code for hidding credentials

--- src/env_variable.py
+++ src/env_variable.py
@@ -1,5 +1,8 @@
-import  os
-from dotenv import find_dotenv, load_dotenv
+import  os # a python library for dealing with environment variables and operating system
+from dotenv import find_dotenv, load_dotenv 
+#dotenv : a python library use to hide credentials.
+# find_dotenv: find automatically a .env file in you project folder and  parents folder
+# load_dotenv: load the variables inside the .env to memory so that the os.getenv can use get them.
 
 DOT_PATH = find_dotenv() # this line work in in background finding the .env file whereever it is.
 load_dotenv(DOT_PATH) 
